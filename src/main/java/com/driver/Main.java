aaec8b19cfee04d5316484a71b6128681bac8917
Maeinukhan <khanmaeinuddin19@gmail.com>
2024-05-31 17:43:32 +0530

    Second project nd change

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -8,14 +8,15 @@ public class Main {
   
 }
 
-public class A{
-    public String meth(){
+ class A{
+     String meth(){
         return "Invoking method from class A";
     }
 }
 
-public class B extends A{
-    public String meth(){
+
+ class B extends A{
+     String meth(){
         return "Method is overridden in Extended class B";
     }
 }
